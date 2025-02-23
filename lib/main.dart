7d289f4ff2431ea9306de0a38ff0bc4f4a2ede9e
Alexandros Mentoudakis <a.mendoudakis@gmail.com>
2025-02-23 13:55:04 +0200

    latest commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:daily_expense_tracker/utils/constants/web_server_uri_constants.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -5,6 +6,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:daily_expense_tracker/widgets/_myApp/my_app.dart';
 
 void main() {
+  WebServer.setServerUri("https://daily-expenses-tracker-backend.vercel.app/");
+
   runApp(
     const ProviderScope(
       child: MyApp(),
--- lib/utils/constants/web_server_uri_constants.dart
+++ lib/utils/constants/web_server_uri_constants.dart
@@ -1,7 +1,7 @@
 class WebServer {
-  // static const String serverUri =
-  //     "http://192.168.1.199:8080"; // macbook - cosmote
-  // static const String serverUri =
-  // "http://192.168.31.50:8080"; // macbook - xiamoi
-  static const String serverUri = "http://10.0.22.253:8080"; // pc - hmu
+  static late String serverUri;
+
+  static void setServerUri(String newUri) {
+    serverUri = newUri;
+  }
 }
